9c2a3696c4560587576085384628b9a892fccd97
Manuri <amaya.uom@gmail.com>
2019-03-01 11:58:52 +0530

    Enable aws-cli resource deletion

--- data-integration/destroy-cluster.sh
+++ data-integration/destroy-cluster.sh
@@ -36,22 +36,22 @@ echo "rds deletion triggered"
 #delete cluster resources
 cluster_name=${infra_cleanup_config[ClusterName]}
 
-#aws cloudformation delete-stack --stack-name "${cluster_name}-worker-nodes"
-#aws eks delete-cluster --name "${cluster_name}"
-#aws eks wait cluster-deleted --name "${cluster_name}"
-#aws eks describe-cluster --name "${cluster_name}" --query "cluster.status"
-#
-#aws cloudformation delete-stack --stack-name=EKS-$cluster_name-ControlPlane
-#aws cloudformation wait stack-delete-complete --stack-name=EKS-$cluster_name-ControlPlane
-#
-#aws cloudformation delete-stack --stack-name=EKS-$cluster_name-ServiceRole
-#aws cloudformation wait stack-delete-complete --stack-name=EKS-$cluster_name-ServiceRole
-#
-#aws cloudformation delete-stack --stack-name=EKS-$cluster_name-DefaultNodeGroup
-#aws cloudformation wait stack-delete-complete --stack-name=EKS-$cluster_name-DefaultNodeGroup
-#
-#aws cloudformation delete-stack --stack-name=EKS-$cluster_name-VPC
+aws cloudformation delete-stack --stack-name "${cluster_name}-worker-nodes"
+aws eks delete-cluster --name "${cluster_name}"
+aws eks wait cluster-deleted --name "${cluster_name}"
+aws eks describe-cluster --name "${cluster_name}" --query "cluster.status"
+
+aws cloudformation delete-stack --stack-name=EKS-$cluster_name-ControlPlane
+aws cloudformation wait stack-delete-complete --stack-name=EKS-$cluster_name-ControlPlane
+
+aws cloudformation delete-stack --stack-name=EKS-$cluster_name-ServiceRole
+aws cloudformation wait stack-delete-complete --stack-name=EKS-$cluster_name-ServiceRole
+
+aws cloudformation delete-stack --stack-name=EKS-$cluster_name-DefaultNodeGroup
+aws cloudformation wait stack-delete-complete --stack-name=EKS-$cluster_name-DefaultNodeGroup
+
+aws cloudformation delete-stack --stack-name=EKS-$cluster_name-VPC
 
 
-eksctl delete cluster --name=$cluster_name
+#eksctl delete cluster --name=$cluster_name
 echo " cluster resources deletion triggered"
